90c0586c73f1e73ac4942ea896c5e7457afa4249
Jordan Cozart <Jordan.Cozart@matrixteam.com>
2023-01-11 09:26:34 -0600

    first commit

---
+++ src/routes/crmRoutes.js
@@ -0,0 +1,31 @@
+import { addNewContact, 
+        getContacts, 
+        getContactWithID, 
+        updateContact, 
+        deleteContact } from '../controllers/crmController'
+
+const routes = (app) => {
+    app.route('/contact')
+    // get all contacts
+    .get((req, res, next) => {
+        // middleware
+        console.log(`Request from: ${req.originalUrl}`)
+        console.log(`Request type: ${req.method}`)
+        next()
+    }, getContacts)
+
+    // post new contact
+    .post(addNewContact)
+
+    app.route('/contact/:contactId')
+    // get specific contact
+    .get(getContactWithID)
+
+    // update a contact
+    .put(updateContact)
+
+    // delete a contact
+    .delete(deleteContact)
+}
+
+export default routes
